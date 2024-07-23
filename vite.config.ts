985757b2515f96d306957ba8e9bf319b3d5352ca
Kenny M. Ngoie <mukonkolekngoie@yahoo.com>
2024-07-23 16:32:05 +0200

    more configurations json and ts files

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,36 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, './src'),
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  base: '/Hospital-Website',
+  build: {
+    outDir: 'dist',
+  },
 });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: [
+//       {
+//         find: '@',
+//         replacement: path.resolve(__dirname, './src'),
+//       },
+//     ],
+//   },
+//   base: '/Hospital-Website',
+// });
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 
